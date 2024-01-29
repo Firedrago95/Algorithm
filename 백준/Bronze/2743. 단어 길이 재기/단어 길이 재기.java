93895be94e95146e7c318149cd0b10fc157a2d32
Firedrago95 <138676687+Firedrago95@users.noreply.github.com>
2024-01-29 20:22:00 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String n = st.nextToken();
+        System.out.println(n.length());
+	}
+}
