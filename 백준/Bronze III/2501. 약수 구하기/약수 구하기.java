a57fe274efa7feebfdfe411c8e1ef8dca4f76aa7
Firedrago95 <138676687+Firedrago95@users.noreply.github.com>
2024-02-15 19:33:02 +0900

    [Bronze III] Title: 약수 구하기, Time: 212 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze III/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] a = new int[k];
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+                if (count == k) {
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+        if (count < k) {
+            System.out.println(0);
+        }
+
+    }
+}
