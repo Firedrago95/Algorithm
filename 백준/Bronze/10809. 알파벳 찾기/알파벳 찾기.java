e3ac2d1d19ede7271438366f3f45b72f4d6f30c5
Firedrago95 <138676687+Firedrago95@users.noreply.github.com>
2024-04-09 18:58:04 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,29 +1,24 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		String str = sc.next();
-		sc.close();
-        
-        int[] answer = new int[26];
-        for (int i=0; i<26; i++) {
-            answer[i] = -1;
-        }
-         
-        for (int i=0; i<str.length(); i++) {
-            char ch = str.charAt(i);
-            
-            if (answer[ch - 'a'] == -1) {
-                answer[ch - 'a'] = i;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] alphabet = new int[26];
+
+        Arrays.fill(alphabet, -1);
+
+        char[] input = br.readLine().toCharArray();
+        for (int i = 0; i < input.length; i++ ) {
+            if (alphabet[input[i] - 'a'] == -1) {
+                alphabet[input[i] - 'a'] = i;
             }
         }
-        
-        for (int i=0; i<26; i++) {
-            System.out.print(answer[i]+" ");
+
+        for (int j : alphabet) {
+            sb.append(j).append(" ");
         }
-        
-        
-	}
+        System.out.println(sb);
+    }
 }
