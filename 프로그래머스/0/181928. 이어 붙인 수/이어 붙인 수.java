fbb94f2890e732efbb748f6fd4f62a65791ad601
Firedrago95 <138676687+Firedrago95@users.noreply.github.com>
2024-04-21 22:47:21 +0900

    [level 0] Title: 이어 붙인 수, Time: 10.10 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String a = "";
+        String b = "";
+        for (int num : num_list) {
+            if (num % 2 == 0) {
+                a += num;
+            } else {
+                b += num;
+            }
+        }
+        return Integer.parseInt(a) + Integer.parseInt(b);
+    }
+}
