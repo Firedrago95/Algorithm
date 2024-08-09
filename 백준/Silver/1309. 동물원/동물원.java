90a2fa7ddc2bd7526f150ddd7518789e73741d14
Firedrago95 <138676687+Firedrago95@users.noreply.github.com>
2024-08-09 20:30:26 +0900

    [Silver I] Title: 동물원, Time: 116 ms, Memory: 18456 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final int MOD = 9901;
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        long[][] dp = new long[n+1][3];
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+        dp[1][2] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % MOD;
+            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % MOD;
+            dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % MOD;
+        }
+        System.out.println((dp[n][0] + dp[n][1] + dp[n][2]) % MOD);
+    }
+}
