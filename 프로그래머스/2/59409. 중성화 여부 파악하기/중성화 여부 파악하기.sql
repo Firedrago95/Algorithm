e9c7a4591785e4e46ac04a728f68cbd8cfa0f43b
Firedrago95 <138676687+Firedrago95@users.noreply.github.com>
2024-09-09 12:41:20 +0900

    Update 중성화 여부 파악하기.sql

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -5,7 +5,7 @@ select
         when SEX_UPON_INTAKE like ('%Neutered%') then 'O'
         when SEX_UPON_INTAKE like ('%Spayed%') then 'O'
         else 'X'
-    end
+    end as '중성화'
 from
     ANIMAL_INS
 order by
