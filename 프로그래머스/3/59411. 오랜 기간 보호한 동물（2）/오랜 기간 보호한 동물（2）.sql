0e7cb83ff28d48d113ab68a542d4e180424a3476
Firedrago95 <138676687+Firedrago95@users.noreply.github.com>
2024-09-09 12:54:37 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,14 @@
+select
+    i.ANIMAL_ID, 
+    i.NAME
+from
+    ANIMAL_INS as i
+join
+    ANIMAL_OUTS as o
+on
+    i.ANIMAL_ID = o.ANIMAL_ID
+order by
+    datediff(o.DATETIME, i.DATETIME) desc
+limit 
+    2
+    
