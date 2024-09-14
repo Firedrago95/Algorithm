9e492402b76bc971a6b83af7c6a6e49d35597752
Firedrago95 <138676687+Firedrago95@users.noreply.github.com>
2024-09-14 15:28:01 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,12 @@
+set @hour = -1;
+select (@hour := @hour + 1) as HOUR,
+    (select
+        count(*)
+    from
+        ANIMAL_OUTS
+    where
+        hour(DATETIME) = @hour) as COUNT
+from
+    ANIMAL_OUTS
+where
+    @hour < 23
