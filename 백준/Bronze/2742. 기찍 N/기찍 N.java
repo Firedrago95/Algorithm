01f8caf12e750157b85f1b1bea92c1ef48a22175
Firedrago95 <138676687+Firedrago95@users.noreply.github.com>
2024-11-12 18:48:29 +0900

    [Bronze IV] Title: 기찍 N, Time: 800 ms, Memory: 31368 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        for (int i = n; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
