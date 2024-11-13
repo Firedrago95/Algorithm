349c9c21369e88122cfc525d18ae9ff11ada48cc
Firedrago95 <138676687+Firedrago95@users.noreply.github.com>
2024-11-13 18:50:08 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 172 ms, Memory: 14896 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
