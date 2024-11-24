9b7a7a1736aca5b75441640b5012d0f2729a564a
Firedrago95 <138676687+Firedrago95@users.noreply.github.com>
2024-11-24 19:57:15 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,14 +1,14 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
         for (int i = 0;  i < absolutes.length; i++) {
             if (!signs[i]) {
-                absolutes[i] = absolutes[i] - (absolutes[i] * 2);
+                answer -= absolutes[i];
+            } else {
+                answer += absolutes[i];
             }
         }
-        long answer = 0;
-        for (int i = 0;  i < absolutes.length; i++) {
-            answer += (long) absolutes[i];
-        }
-        return (int) answer;
+        
+        return answer;
     }
 }
