3cb5bb86cb969cc10344d5aa2188b42f763a504a
Firedrago95 <138676687+Firedrago95@users.noreply.github.com>
2024-11-24 19:44:25 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int num) {
+        int count = 0;
+        long n = (long) num;
+        while (n != 1) {
+            if (count >= 500) return -1;
+            if (n % 2 == 0) {
+                n /= 2;
+                count++;
+                continue;
+            } else {
+                n = (n * 3) + 1;
+                count++;
+                continue;
+            }
+        }
+        return count;
+    }
+}
