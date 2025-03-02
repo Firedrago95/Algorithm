bdd562616698e72a258f958301b79ff7a8fad86b
drago <138676687+Firedrago95@users.noreply.github.com>
2025-03-02 21:07:47 +0900

    [Bronze III] Title: 최댓값, Time: 108 ms, Memory: 14600 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,26 +1,26 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-        int[] a = new int[9];
-        
-		for (int i=0; i<9; i++) {
-            st = new StringTokenizer(br.readLine());
-            a[i] = Integer.parseInt(st.nextToken());
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            list.add(Integer.parseInt(br.readLine()));
         }
-        
+
         int max = 0;
-        int maxIndex =0;
-        for (int i=0; i<9; i++) {
-            if (a[i] > max) {
-                max = a[i];
-                maxIndex = i;
+        int index = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (max < list.get(i)) {
+                max = list.get(i);
+                index = i;
             }
         }
-        System.out.println(max);
-        System.out.println(maxIndex +1);
-	}
+
+        bw.write(String.format("%d%n%d",max, index + 1));
+        br.close();
+        bw.close();
+    }
 }
