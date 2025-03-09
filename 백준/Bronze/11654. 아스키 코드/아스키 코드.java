8ffba3d9247563a00be62dd36b5728fbd47d19c2
drago <138676687+Firedrago95@users.noreply.github.com>
2025-03-09 16:00:52 +0900

    [Bronze V] Title: 아스키 코드, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,11 +1,13 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = br.readLine().charAt(0);
-        System.out.println(a);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int input = br.readLine().charAt(0);
+        bw.write(input + "");
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
