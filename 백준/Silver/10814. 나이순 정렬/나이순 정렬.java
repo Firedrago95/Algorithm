d293870fbcef54c1eb9b2bc0619adccb6f6c97b4
drago <138676687+Firedrago95@users.noreply.github.com>
2025-03-10 17:13:05 +0900

    [Silver V] Title: 나이순 정렬, Time: 752 ms, Memory: 56308 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,47 +1,45 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(st.nextToken());
-        Member[] a = new Member[N];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int age = Integer.parseInt(st.nextToken());
-            String name = st.nextToken();
-            a[i] = new Member(age, name);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        List<Member> members = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            members.add(new Member(Integer.parseInt(input[0]), input[1]));
         }
-        Arrays.sort(a);
 
-        for (Member member : a) {
-            sb.append(member.age).append(" ").append(member.name).append("\n");
+        Collections.sort(members);
+
+        for (Member member : members) {
+            bw.write(member.toString());
         }
-        System.out.println(sb);
-    }
-}
 
-class Member implements Comparable{
-    public int index;
-    public int age;
-    public String name;
-
-    public Member(int age, String name) {
-        this.age = age;
-        this.name = name;
+        br.close();
+        bw.close();
     }
 
-    @Override
-    public int compareTo(Object o) {
-        Member member = null;
-        if (o instanceof Member) {
-            member = (Member)o;
+    public static class Member implements Comparable<Member> {
+        private int age;
+        private String name;
+
+        public Member(int age, String name) {
+            this.age = age;
+            this.name = name;
         }
-        if (age == member.age) {
-            return index - member.index;
+
+        @Override
+        public int compareTo(Member o) {
+           return this.age - o.age;
         }
-        return age - member.age;
+
+        @Override
+        public String toString() {
+            return age + " " + name + "\n";
+        }
     }
 }
