2aefba94132c2e7f41c8dcd72a40cd07ccf68b95
drago <138676687+Firedrago95@users.noreply.github.com>
2025-03-10 16:23:00 +0900

    [Silver V] Title: 단어 정렬, Time: 400 ms, Memory: 24016 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,33 +1,25 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        List<String> a = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            String word = br.readLine();
-            if (!a.contains(word)) {
-                a.add(word);
-            }
-        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Collections.sort(a, (a1, a2) ->{
-            if (a1.length() == a2.length()) {
-                for (int i = 0; i < a1.length(); i++) {
-                    if (a1.charAt(i) != a2.charAt(i)) {
-                        return a1.charAt(i) - a2.charAt(i);
-                    }
-                }
-            }
-            return a1.length() - a2.length();
-        });
-
-        for (String s : a) {
-            sb.append(s).append("\n");
+        int n = Integer.parseInt(br.readLine());
+        List<String> words = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            words.add(br.readLine());
         }
-        System.out.println(sb);
+        
+        words = new ArrayList<>(new HashSet(words));
+
+        Collections.sort(words, Comparator.comparing(String::length).thenComparing(String::compareTo));
+
+        for (String word : words) {
+            bw.write(word + "\n");
+        }
+        br.close();
+        bw.close();
     }
 }
