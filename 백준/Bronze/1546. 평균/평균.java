1c1fa2cc55e6edd8406ba6b32ec98a0b5f701251
drago <138676687+Firedrago95@users.noreply.github.com>
2025-03-17 15:28:49 +0900

    [Bronze I] Title: 평균, Time: 76 ms, Memory: 12000 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,17 +1,25 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int n = Integer.parseInt(br.readLine());
-        int[] nums = new int[n];
-        String[] input = br.readLine().split(" ");
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = Integer.parseInt(input[i]);
+        String[] str = br.readLine().split(" ");
+
+        int max = 0;
+        int sum = 0;
+        for(int i = 0; i<n; i++){
+            int cur = Integer.parseInt(str[i]);
+            sum += cur;
+            max = max < cur ? cur : max;
         }
 
-        int max = Arrays.stream(nums).max().getAsInt();
-        int sum = Arrays.stream(nums).sum();
-        System.out.println(sum * 100.0 / max / n);
+        float rate = 100 / (float)max;
+        bw.write(sum * rate / n + "");
+        br.close();
+        bw.close();
     }
 }
