93420a11aa3c246df25bc9dd64a9ef9d430e9b14
drago <138676687+Firedrago95@users.noreply.github.com>
2025-11-26 21:18:28 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1356 ms, Memory: 129548 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,34 +1,37 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int dimension = Integer.parseInt(stringTokenizer.nextToken());
-        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
-        long a[][] = new long[dimension + 1][dimension + 1];
-        for (int i = 1; i <= dimension; i++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            for (int j = 1; j <= dimension; j++) {
-                a[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] a = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                a[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        long[][] sum = new long[dimension + 1][dimension + 1];
-        for (int i = 1; i <= dimension; i++) {
-            for (int j = 1; j <= dimension; j++) {
-                sum[i][j] = sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1] + a[i][j];
+
+        int[][] s = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                s[i][j] = s[i-1][j] + s[i][j-1] - s[i-1][j-1] + a[i][j];
             }
         }
-        for (int i = 1; i <= quizNo; i++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int x1 = Integer.parseInt(stringTokenizer.nextToken());
-            int y1 = Integer.parseInt(stringTokenizer.nextToken());
-            int x2 = Integer.parseInt(stringTokenizer.nextToken());
-            int y2 = Integer.parseInt(stringTokenizer.nextToken());
-            
-            long result = sum[x2][y2] - sum[x1-1][y2] - sum[x2][y1 - 1] + sum[x1-1][y1-1];
-            System.out.println(result);
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            System.out.println(s[x2][y2] - s[x2][y1 - 1] - s[x1 - 1][y2] + s[x1 - 1][y1 - 1]);
         }
-	}
+    }
 }
