2d51ea1a624aa24aeb23886eab735c535a6d1940
drago <138676687+Firedrago95@users.noreply.github.com>
2025-12-04 13:59:33 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,23 +1,18 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        char[] a = br.readLine().toCharArray();
-        int max = 0;
-        for (int i = 0; i < a.length - 1; i++) {
-            for (int j = 0; j < a.length - i - 1; j++) {
-                if (a[j] < a[j + 1]) {
-                    char tmp = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = tmp;
-                }
-            }
+        
+        char[] nums = br.readLine().toCharArray();
+
+        Arrays.sort(nums);
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            sb.append(nums[i]);
         }
-        for (char num : a) {
-            System.out.print(num);
-        }
+        System.out.println(sb);
     }
 }
