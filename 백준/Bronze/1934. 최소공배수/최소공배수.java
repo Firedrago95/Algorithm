ca37f63397d8b42cfc85a91c9666228ae8e9da6e
drago <138676687+Firedrago95@users.noreply.github.com>
2025-12-17 11:50:58 +0900

    [Bronze I] Title: 최소공배수, Time: 148 ms, Memory: 15048 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,24 +1,29 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static StringBuffer sb = new StringBuffer();
+
     public static void main(String[] args) throws IOException {
-        int N = Integer.parseInt(br.readLine());
-        while (N-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int t = Integer.parseInt(st.nextToken());
+
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int gcd = 0;
 
-            for (int i = 1; i <= Math.min(a,b); i++) {
-                if (a % i == 0 && b % i == 0) {
-                    gcd = i;
-                }
-            }
+            int gcd = calculate(b, a);
+
             System.out.println(a * b / gcd);
         }
+    }
 
+    private static int calculate(int b, int a) {
+        if (a == 0) return b;
+
+        int c = b % a;
+        return calculate(a, c);
     }
 }
