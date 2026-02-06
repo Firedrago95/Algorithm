723eaa86dd5be5a608c616216c8bbe7ca6db198a
drago <138676687+Firedrago95@users.noreply.github.com>
2026-02-07 04:56:29 +0900

    [Gold V] Title: 트리, Time: 92 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,55 +1,46 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    static StringTokenizer st;
-    static int N;
-    static boolean[] visit;
+    static ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
     static int answer = 0;
-    static int deleteNode = 0;
-    static ArrayList<Integer>[] tree;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-        tree = new ArrayList[N];
-        visit = new boolean[N];
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
         int root = 0;
-        for (int i = 0; i < N; i++) {
-            tree[i] = new ArrayList<>();
-        }
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int p = Integer.parseInt(st.nextToken());
-            if (p != -1) {
-                tree[i].add(p);
-                tree[p].add(i);
-            } else {
+        for(int i=0;i<=N;i++)
+            tree.add(new ArrayList<>());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0;i<N;i++){
+            int node = Integer.parseInt(st.nextToken());
+            if(node == -1){
                 root = i;
+                continue;
             }
+            tree.get(node).add(i);
         }
-        deleteNode = Integer.parseInt(br.readLine());
-        if (deleteNode == root) {
-            System.out.println(0);
-        } else {
-            dfs(root);
-            System.out.println(answer);
-        }
+        int remove = Integer.parseInt(br.readLine());
+        if(remove == root)
+            answer = 0;
+        else
+            search(remove, root);
+        bw.write(answer + "");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    private static void dfs(int number) {
-        visit[number] = true;
-        int cNode = 0;
-        for (int i : tree[number]) {
-            if (!visit[i] && i != deleteNode) {
-                cNode++;
-                dfs(i);
-            }
-        }
-        if (cNode == 0) {
+    static void search(int remove, int node){
+        if(tree.get(node).contains(remove))
+            tree.get(node).remove(Integer.valueOf(remove));
+ 
+        if(tree.get(node).isEmpty()){
             answer++;
+            return;
+        }
+        for(int next : tree.get(node)){
+            search(remove, next);
         }
     }
 }
