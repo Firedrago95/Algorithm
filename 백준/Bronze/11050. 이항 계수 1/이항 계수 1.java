ef6164fbe849297308fbb10fabb83c8adb6abdd5
drago <138676687+Firedrago95@users.noreply.github.com>
2026-02-10 09:46:11 +0900

    [Bronze I] Title: 이항 계수 1, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        dp = new int[N + 1][K + 1];
+        
+        System.out.println(combination(N, K));
+    }
+
+    private static int combination(int n, int k) {
+        if (n == k || k == 0) return 1;
+        
+        if (dp[n][k] > 0) return dp[n][k];
+        
+        return dp[n][k] = combination(n - 1, k - 1) + combination(n - 1, k);
+    }
+}
