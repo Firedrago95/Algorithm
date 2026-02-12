6283a66d9c4a464d887ffedd1ffa02fe5e9e8f4c
drago <138676687+Firedrago95@users.noreply.github.com>
2026-02-12 14:20:44 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,18 +1,29 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder sb;
+
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        long[][] dp = new long[91][2];
-        dp[1][0] = 0;
-        dp[1][1] = 1;
-        dp[2][0] = 1;
-        dp[2][1] = 0;
-        for (int i = 3; i <=n; i++) {
-            dp[i][0] = dp[i-1][0] + dp[i-1][1];
-            dp[i][1] = dp[i-1][0];
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        if (n < 3) {
+            System.out.println(1);
+            return;
         }
-        System.out.println(dp[n][0] + dp[n][1]);
+
+        long[] d = new long[n + 1];
+        d[1] = 1;
+        d[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            d[i] = d[i - 1] + d[i - 2];
+        }
+        System.out.println(d[n]);
     }
 }
