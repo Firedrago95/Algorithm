64e19e35db6187b4d3acaa1131073ec4a3daf2ff
drago <138676687+Firedrago95@users.noreply.github.com>
2026-02-13 11:25:11 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 92 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,29 +1,38 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static final int MOD = 1000000000;
+
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder sb;
+    static long[][] d;
+    static long MOD = 1000000000;
 
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        long[][] dp = new long[n+1][10];
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
-        for (int i = 1; i < 10; i++) {
-            dp[1][i] = 1;
+        int n = Integer.parseInt(st.nextToken());
+
+        d = new long[n + 1][10];
+
+        for (int i = 1; i <= 9; i++) {
+            d[1][i] = 1;
         }
 
-        for (int i = 2; i <= n; i++) {
-            dp[i][0] = dp[i-1][1] % MOD;
-            dp[i][9] = dp[i-1][8] % MOD;
-            for (int j = 1; j < 9; j++) {
-                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % MOD;
+        for (int i = 2; i <= n; i ++) {
+            d[i][0] = d[i - 1][1] % MOD;
+            d[i][9] = d[i - 1][8] % MOD;
+            for (int j = 1; j <= 8; j++) {
+                d[i][j] = (d[i - 1][j - 1] + d[i - 1][j + 1]) % MOD;
             }
         }
-
-        long result = 0;
-        for (int i = 0; i < 10; i++) {
-            result = (result + dp[n][i]) % MOD;
+        long sum = 0;
+        for (int i = 0; i <= 9; i++) {
+            sum += (d[n][i] % MOD);
         }
-        System.out.println(result);
+        System.out.println(sum % MOD);
     }
 }
