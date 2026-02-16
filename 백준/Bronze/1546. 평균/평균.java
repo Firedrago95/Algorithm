27cc1c46223f6b717ce64972f4c802ee634b931c
drago <138676687+Firedrago95@users.noreply.github.com>
2026-02-16 15:39:39 +0900

    [Bronze I] Title: 평균, Time: 108 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,24 +1,17 @@
 import java.util.*;
 import java.io.*;
-import java.util.stream.Collectors;
-
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        List<Integer> nums = Arrays.stream(br.readLine().split(" "))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-        long sum = 0;
-        long max = 0;
-
-        for (int num : nums) {
-            if (max < num) max = num;
-            sum += num;
+        int[] nums = new int[n];
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(input[i]);
         }
 
+        int max = Arrays.stream(nums).max().getAsInt();
+        int sum = Arrays.stream(nums).sum();
         System.out.println(sum * 100.0 / max / n);
     }
 }
