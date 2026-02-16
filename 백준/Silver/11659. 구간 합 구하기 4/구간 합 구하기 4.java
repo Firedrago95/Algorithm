ed0bbd9bbf8eb451f400a1ee0f96abbb62a09cc6
drago <138676687+Firedrago95@users.noreply.github.com>
2026-02-16 15:48:33 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 580 ms, Memory: 55252 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,26 +1,42 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder sb;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
         int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
 
+        int[] nums = new int[n];
+        long[] sum = new long[n];
         st = new StringTokenizer(br.readLine());
-        int[] sums = new int[n + 1];
-        for (int i = 1; i < sums.length; i++) {
-            sums[i] = sums[i - 1] + Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+            if (i >= 1) {
+                sum[i] = sum[i - 1] + nums[i];
+            } else {
+                sum[i] = nums[i];
+            }
         }
 
-        while (m-- > 0) {
+        while (q-- > 0) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            System.out.println(sums[end] - sums[start - 1]);
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            if (i >= 2) {
+                sb.append(sum[j - 1] - sum[i - 2]).append("\n");
+            } else {
+                sb.append(sum[j - 1]).append("\n");
+            }
         }
+        System.out.println(sb);
     }
 }
