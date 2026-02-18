00168e72246c47610cff5022bae92f07e8958479
drago <138676687+Firedrago95@users.noreply.github.com>
2026-02-18 14:10:10 +0900

    [Gold II] Title: 버블 소트, Time: 1320 ms, Memory: 85904 KB -BaekjoonHub

--- 백준/Gold/1377. 버블 소트/버블 소트.java
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -1,40 +1,46 @@
-import java.util.*;
 import java.io.*;
-
-class Point implements Comparable<Point>{
-    int num;
-    int idx;
-
-    public Point(int num, int idx) {
-        this.num = num;
-        this.idx = idx;
-    }
-
-    @Override
-    public int compareTo(Point o) {
-        return this.num - o.num;
-    }
-}
+import java.util.*;
 
 public class Main {
+
+    static BufferedReader br;
+    static StringBuilder sb;
+    static StringTokenizer st;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
+        Mdata[] a = new Mdata[n];
 
-        Point[] points = new Point[n];
-        for (int i = 0; i < points.length; i++) {
-            int num = Integer.parseInt(br.readLine());
-            points[i] = new Point(num, i);
+        for (int i = 0; i < n; i++) {
+            a[i] = new Mdata(Integer.parseInt(br.readLine()), i);
         }
 
-       Arrays.sort(points);
+        Arrays.sort(a);
 
         int max = 0;
-        for (int i = 0; i < points.length; i++) {
-            max = Math.max(max, points[i].idx - i);
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, a[i].index - i);
         }
 
         System.out.println(max + 1);
+    }
+
+    static class Mdata implements Comparable<Mdata> {
+
+        public int value;
+        public int index;
+
+        public Mdata(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+
+        @Override
+        public int compareTo(Mdata o) {
+            return this.value - o.value;
+        }
     }
 }
