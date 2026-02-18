2155a90f5246daa4cee588d629c9e78ddf1f063c
drago <138676687+Firedrago95@users.noreply.github.com>
2026-02-18 14:45:52 +0900

    [Silver V] Title: K번째 수, Time: 2132 ms, Memory: 381036 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -1,47 +1,28 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+
+    static BufferedReader br;
+    static StringBuilder sb;
+    static StringTokenizer st;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
+        int[] a = new int[n];
         st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
         }
 
-        quickSort(arr, 0, n - 1);
-        System.out.println(arr[k-1]);
-    }
+        Arrays.sort(a);
 
-    private static void quickSort(int[] arr, int start, int end) {
-        int pivot = arr[(start + end) / 2];
-
-        int left = start;
-        int right = end;
-
-        while (left <= right) {
-            while (arr[left] < pivot) left++;
-            while (arr[right] > pivot) right--;
-
-            if (left <= right) {
-                int tmp = arr[left];
-                arr[left] = arr[right];
-                arr[right] = tmp;
-                left++;
-                right--;
-            }
-        }
-        if (start < right) {
-            quickSort(arr, start, right);
-        }
-        if (left < end) {
-            quickSort(arr, left, end);
-        }
+        System.out.println(a[k-1]);
     }
 }
