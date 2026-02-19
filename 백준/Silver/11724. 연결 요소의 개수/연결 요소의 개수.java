2d4306d275f8bfa7cd1e21e5e72bf1705ce182f8
drago <138676687+Firedrago95@users.noreply.github.com>
2026-02-19 21:20:07 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 532 ms, Memory: 141904 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,35 +1,36 @@
-import java.util.*;
 import java.io.*;
+import java.sql.Array;
+import java.util.*;
 
 public class Main {
 
-    static ArrayList<ArrayList<Integer>> a;
+    static BufferedReader br;
+    static StringTokenizer st;
     static boolean[] visited;
-    static int n;
-    static int m;
+    static List<Integer>[] a;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        
-        a = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            a.add(new ArrayList<>());
-        }
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        a = new ArrayList[n + 1];
         visited = new boolean[n + 1];
-        
+        for (int i = 1; i <= n; i++) {
+            a[i] = new ArrayList<>();
+        }
+
         for (int i = 1; i <= m; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            a.get(u).add(v);
-            a.get(v).add(u);
+            a[u].add(v);
+            a[v].add(u);
         }
-        
-        int count = 0;
+
+        long count = 0;
         for (int i = 1; i <= n; i++) {
             if (!visited[i]) {
                 count++;
@@ -38,14 +39,16 @@ public class Main {
         }
         System.out.println(count);
     }
-    
+
     private static void dfs(int i) {
+        if (visited[i]) return;
+
         visited[i] = true;
-        
-        for (int next : a.get(i)) {
-            if (!visited[next]) {
-                dfs(next);
-            } 
+
+        for (Integer n : a[i]) {
+            if (!visited[n]) {
+                dfs(n);
+            }
         }
     }
 }
