445c32f9790832b9a7e6367e7d6fc8a156739aa6
drago <138676687+Firedrago95@users.noreply.github.com>
2026-02-21 16:01:13 +0900

    [Silver III] Title: 소수 구하기, Time: 268 ms, Memory: 23792 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,29 +1,38 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder sb;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
 
-        for (int i = M; i <= N; i++) {
-            if (check(i)) {
+        int[] a = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            a[i] = i;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (a[i] == 0) continue;
+
+            for (int j = i + i; j <= n; j = j + i) {
+                a[j] = 0;
+            }
+        }
+
+        for (int i : a) {
+            if (i >= m && i > 0) {
                 sb.append(i).append("\n");
             }
         }
         System.out.println(sb);
-    }
-
-    private static boolean check(int num) {
-        if (num == 1) return false;
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) return false;
-        }
-        return true;
     }
 }
