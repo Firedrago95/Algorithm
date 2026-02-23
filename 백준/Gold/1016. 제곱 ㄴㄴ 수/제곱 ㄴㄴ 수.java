351f39627e9b06a2adcb0029056583f0cbdd9c71
drago <138676687+Firedrago95@users.noreply.github.com>
2026-02-23 12:30:48 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 144 ms, Memory: 15404 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -1,36 +1,33 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
+    static BufferedReader br;
+    static StringTokenizer st;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
 
         long min = Long.parseLong(st.nextToken());
         long max = Long.parseLong(st.nextToken());
 
-       int length = (int) (max - min + 1);
+        boolean[] check = new boolean[(int) (max - min + 1)];
 
-        boolean[] isNotSquare = new boolean[length];
         for (long i = 2; i * i <= max; i++) {
-            long square = i * i;
+            long pow = i * i;
+            long start_index = min / pow;
+            if (min % pow != 0) start_index++;
 
-            long start = min / square;
-            if (min % square != 0) {
-                start++;
-            }
-
-            for (long j = start; j * square <= max; j++) {
-                long current = j * square;
-                isNotSquare[(int) (current - min)] = true;
+            for (long j = start_index; pow * j <= max; j++) {
+                check[(int) ((j * pow) - min)] = true;
             }
         }
+
         int count = 0;
-        for (boolean b : isNotSquare) {
-            if (!b) {
-                count++;
-            }
+        for (boolean b : check) {
+            if (!b) count++;
         }
         System.out.println(count);
     }
