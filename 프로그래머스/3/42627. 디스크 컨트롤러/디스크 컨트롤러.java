bbf0aa16c85577169a5968fee5a477c90f3829a1
drago <138676687+Firedrago95@users.noreply.github.com>
2026-02-27 12:08:00 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.20 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] jobs) {
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        
+        int count = 0;
+        int jobIndex = 0;
+        int currentTime = 0;
+        int totalWait = 0;
+        
+        while (count < jobs.length) {
+            while (jobIndex < jobs.length && jobs[jobIndex][0] <= currentTime) {
+                pq.add(jobs[jobIndex++]);
+            }
+            
+            if (!pq.isEmpty()) {
+                int[] job = pq.poll();
+                totalWait += (currentTime + job[1]) - job[0];
+                currentTime += job[1];
+                count++;
+            } else {
+                currentTime = jobs[jobIndex][0];
+            }
+        }
+        
+        return totalWait / jobs.length;
+    }
+}
