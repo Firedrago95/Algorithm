994d66c250e1a18f0db2de62b832b29716becaa8
drago <138676687+Firedrago95@users.noreply.github.com>
2026-03-01 20:43:36 +0900

    [level 3] Title: N으로 표현, Time: 27.61 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,36 +1,37 @@
 import java.util.*;
 class Solution {
     public int solution(int N, int number) {
-        Set<Integer>[] nc = new HashSet[9];
-        for (int i = 0; i < 9; i++) {
-            nc[i] = new HashSet();
+        Set<Integer>[] set = new Set[9];
+        
+        for (int i = 1; i <= 8; i++) {
+            set[i] = new HashSet<>();
         }
         
         for (int i = 1; i <= 8; i++) {
-            nc[i].add(getSequenceNum(N, i));
+            set[i].add(getNM(N, i));
             
-            for (int k = 1; k < i; k++) {
-                int j = i - k;
-                
-                for (int a : nc[k]) {
-                    for (int b : nc[j]) {
-                        nc[i].add(a + b);
-                        nc[i].add(a - b);
-                        nc[i].add(a * b);
-                        if (b != 0) nc[i].add(a / b);
+            for (int j = 1; j < i; j++) {
+                int k = i - j;
+                for (int s : set[j]) {
+                    for (int f : set[k]) {
+                        set[i].add(f + s);
+                        set[i].add(f * s);
+                        set[i].add(f - s);
+                        if (s != 0) set[i].add(f / s);
                     }
                 }
             }
-            if (nc[i].contains(number)) return i;
+            
+            if (set[i].contains(number)) return i;
         }
         return -1;
     }
     
-    private int getSequenceNum(int N, int k) {
-        int num = 0;
-        while (k-- > 0) {
-            num = num * 10 + N;
+    private int getNM(int N, int count) {
+        int a = 0;
+        while(count-- > 0) {
+            a = a * 10 + N;
         }
-        return num;
+        return a;
     }
 }
