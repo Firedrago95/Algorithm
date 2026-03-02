d3a6e7724cf46f3e32c4b4fb3f2856c4a97e7bca
drago <138676687+Firedrago95@users.noreply.github.com>
2026-03-02 15:27:38 +0900

    [level 3] Title: 입국심사, Time: 47.09 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,25 +1,30 @@
 import java.util.*;
 
 class Solution {
-    public long solution(int n, int[] times) { 
-        Arrays.sort(times);
+    public long solution(int n, int[] times) {
         long min = 1;
-        long max = (long) n * times[times.length - 1];
+        long max = 0;
+        long answer = 0;
+        for (int time : times) {
+            max = Math.max(max, time);
+        }
+        max *= n;
         
-        while (min < max) {
-            long mid = min + ((max - min) / 2);
+        while (min <= max) {
+            long mid = min + (max - min) / 2;
             long possible = 0;
             for (int time : times) {
                 possible += mid / time;
+                if (possible >= n) break;
             }
             
             if (possible < n) {
                 min = mid + 1;
             } else {
-                max = mid;
+                answer = mid;
+                max = mid - 1;
             }
         }
-        
-        return min;
+        return answer;
     }
 }
