cf43f9546c8ab769cede73736d16dd6986fad7ab
drago <138676687+Firedrago95@users.noreply.github.com>
2026-03-03 12:31:40 +0900

    [level 3] Title: 가장 먼 노드, Time: 30.41 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,34 +1,33 @@
 import java.util.*;
 class Solution {
-    
-    ArrayList<Integer>[] graph;
+    List<Integer>[] arr;
     boolean[] visited;
     public int solution(int n, int[][] edge) {
-        this.graph = new ArrayList[n + 1];
         this.visited = new boolean[n + 1];
+        this.arr = new ArrayList[n + 1];
         
         for (int i = 0; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+            arr[i] = new ArrayList<>();
         }
         
         for (int i = 0; i < edge.length; i++) {
             int a = edge[i][0];
             int b = edge[i][1];
-            graph[a].add(b);
-            graph[b].add(a);
+            arr[a].add(b);
+            arr[b].add(a);
         }
         
         Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
         visited[1] = true;
         int answer = 0;
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int size = q.size();
             answer = size;
             
             for (int i = 0; i < size; i++) {
                 int poll = q.poll();
-                for (int node : graph[poll]) {
+                for (int node : arr[poll]) {
                     if (!visited[node]) {
                         visited[node] = true;
                         q.add(node);
@@ -36,6 +35,7 @@ class Solution {
                 }
             }
         }
+        
         return answer;
     }
 }
