78a1285a4907d8d92c1b14f680738d3fb72517c8
drago <138676687+Firedrago95@users.noreply.github.com>
2026-03-03 14:05:37 +0900

    [level 2] Title: 기능개발, Time: 3.06 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -7,21 +7,19 @@ class Solution {
             int p = 100 - progresses[i];
             int s = speeds[i];
             
-           q.add((p + (s - 1)) / s);
+            q.add((p + (s - 1)) / s);
         }
         
         List<Integer> l = new ArrayList<>();
-        while (q.size() > 1) {
-            int count = 0;
+        while (!q.isEmpty()) {
             int d = q.poll();
-            count++;
+            int count = 1;
             while (!q.isEmpty() && q.peek() <= d) {
                 q.poll();
                 count++;
             }
             l.add(count);
         }
-        if (!q.isEmpty()) l.add(1);
         
         return l.stream().mapToInt(Integer::intValue).toArray();
     }
