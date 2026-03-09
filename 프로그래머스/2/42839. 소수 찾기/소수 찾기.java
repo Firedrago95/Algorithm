5c4a08c67b5fc3ae4a2e3881c6161a43f9b99f94
drago <138676687+Firedrago95@users.noreply.github.com>
2026-03-09 11:23:12 +0900

    [level 2] Title: 소수 찾기, Time: 25.50 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,36 +1,40 @@
 import java.util.*;
 class Solution {    
-    Set<String> set = new HashSet<>();
+    Set<Integer> set = new HashSet<>();
     boolean[] visited;
     public int solution(String numbers) {
         this.visited = new boolean[numbers.length()];
         
-        backtrack(numbers, "");
-        System.out.println(set);
-        return set.size();
+        backTrack(numbers, "");
+        
+        int count = 0;
+        for (int n : set) {
+            if (isPrime(n)) {
+                count++;
+            }
+        }
+        return count;
     }
     
-    private void backtrack(String input, String answer) {
-        if (answer != null && check(answer)) {
-            set.add(answer);
+    public void backTrack(String numbers , String input) {
+        if (!input.isEmpty()) {
+            set.add(Integer.parseInt(input));
         }
         
-        for (int i = 0; i < input.length(); i++) {
+        for (int i = 0; i < numbers.length(); i++) {
             if (!visited[i]) {
-                
                 visited[i] = true;
-                backtrack(input, answer + input.charAt(i));
+                backTrack(numbers, input + numbers.charAt(i));
                 visited[i] = false;
             }
         }
     }
     
-    private boolean check(String n) {
-        if (n == null || n.isEmpty() || n.equals("1") || n.startsWith("0")) return false;
-        int num = Integer.parseInt(n);
+    public boolean isPrime (int n) {
+        if (n < 2) return false;
         
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
         }
         return true;
     }
