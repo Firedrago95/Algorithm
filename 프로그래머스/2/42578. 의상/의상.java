c2f864970c23c44e7dc4aefdb6ef2110c37a45b4
drago <138676687+Firedrago95@users.noreply.github.com>
2026-03-11 13:44:43 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,8 +2,11 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> map = new HashMap<>();
+        
         for (String[] c : clothes) {
+            String item = c[0];
             String category = c[1];
+            
             map.put(category, map.getOrDefault(category, 0) + 1);
         }
         
