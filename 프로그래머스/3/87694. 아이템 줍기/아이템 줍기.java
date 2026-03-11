1ccef7ce30355023ef7f1cabc518b1dbeb602b60
drago <138676687+Firedrago95@users.noreply.github.com>
2026-03-11 11:22:46 +0900

    [level 3] Title: 아이템 줍기, Time: 0.78 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,50 +1,44 @@
 import java.util.*;
 class Solution {
-    int[][] map = new int[102][102];
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        for (int[] rect : rectangle) {
-            int x1 = rect[0] * 2, y1 = rect[1] * 2;
-            int x2 = rect[2] * 2, y2 = rect[3] * 2;
+        int[][] map = new int[102][102];
+        
+        for (int[] r : rectangle) {
+            int x1 = r[0] * 2, y1 = r[1] * 2;
+            int x2 = r[2] * 2, y2 = r[3] * 2;
             
             for (int i = x1; i <= x2; i++) {
                 for (int j = y1; j <= y2; j++) {
-                    if (i > x1 && i < x2 && j > y1 && j < y2) {
+                    if (i > x1 && i < x2 && y1 < j && j < y2) {
                         map[i][j] = 2;
-                    }
-                    else if (map[i][j] != 2) {
+                    } else if (map[i][j] != 2) {
                         map[i][j] = 1;
                     }
                 }
             }
         }
-        return bfs(characterX * 2, characterY * 2, itemX * 2, itemY * 2);
-    }
-    
-    private int bfs(int startX, int startY, int targetX, int targetY) {
         int[] dx = new int[] {1,0,-1,0};
         int[] dy = new int[] {0,1,0,-1};
-        
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{startX, startY, 0});
-        
         boolean[][] visited = new boolean[102][102];
+        q.add(new int[]{characterX * 2, characterY * 2, 0});
+        visited[characterX * 2][characterY * 2] = true;
         
-        while (!q.isEmpty()) {
-            int[] poll = q.poll();
+        while(!q.isEmpty()) {
+            int[] current = q.poll();
+            int cx = current[0];
+            int cy = current[1];
+            int d = current[2];
             
-            if (poll[0] == targetX && poll[1] == targetY) {
-                return poll[2] / 2;
-            }
+            if (cx == itemX * 2 && cy == itemY * 2) return d / 2;
             
             for (int i = 0; i < 4; i++) {
-                int nx = poll[0] + dx[i];
-                int ny = poll[1] + dy[i];
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
                 
-                if (nx >= 0 && nx < 102 && ny >= 0 && ny < 102) {
-                    if (map[nx][ny] == 1 && !visited[nx][ny]) {
-                        visited[nx][ny] = true;
-                        q.add(new int[]{nx, ny, poll[2] + 1});
-                    }
+                if (nx >= 0 && nx < 102 && ny >= 0 && ny < 102 && map[nx][ny] == 1 && !visited[nx][ny]) {
+                    q.add(new int[]{nx, ny, d + 1});
+                    visited[nx][ny] = true;
                 }
             }
         }
