6e1a71ddf73382c6abe80de2b87086399c55a76a
drago <138676687+Firedrago95@users.noreply.github.com>
2026-03-11 13:33:41 +0900

    [level 3] Title: 순위, Time: 12.02 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,32 +1,31 @@
 class Solution {
     public int solution(int n, int[][] results) {
-        int[][] b = new int[n + 1][n + 1];
+        boolean[][] r = new boolean[n + 1][n + 1];
         
-        for (int[] r : results) {
-            int win = r[0];
-            int loose = r[1];
-            b[win][loose] = 1;
+        for (int[] result : results) {
+            int win = result[0];
+            int loose = result[1];
+            r[win][loose] = true;
         }
         
         for (int k = 1; k <= n; k++) {
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= n; j++) {
-                    if (b[i][k]  == 1 && b[k][j] == 1) {
-                        b[i][j] = 1;
-                    } 
+                    if (r[i][k] && r[k][j]) {
+                        r[i][j] = true;
+                    }
                 }
             }
         }
-        
-        int answer = 0;
+        int result = 0;
         for (int i = 1; i <= n; i++) {
             int count = 0;
             for (int j = 1; j <= n; j++) {
-                if (b[i][j] == 1 || b[j][i] == 1) count++;
+                if (r[i][j] || r[j][i]) count++;
             }
             
-            if (count == n -1) answer++;
+            if (count == n - 1) result++;
         }
-        return answer;
+        return result;
     }
 }
