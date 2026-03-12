3607911f04f09a243c1e944811df2af29428c0c8
drago <138676687+Firedrago95@users.noreply.github.com>
2026-03-12 13:18:02 +0900

    [level 2] Title: 프로세스, Time: 1.42 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,24 +1,22 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        Deque<int[]> q = new ArrayDeque<>();
-        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int i = 0; i < priorities.length; i++) {
-            q.add(new int[]{priorities[i], i});
-            pq.add(priorities[i]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int p : priorities) {
+            pq.add(p);
         }
         int count = 0;
-        while (true) {
-            if (q.peek()[0] < pq.peek()) {
-                q.add(q.poll());
-            } else {
-                count++;
-                int[] poll = q.poll();
-                pq.poll();
-                if (poll[1] == location) {
-                    return count;
+        while(!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (pq.peek() == priorities[i]) {
+                    count++;
+                    pq.poll();
+                    if (i == location) {
+                        return count;
+                    }
                 }
             }
         }
+        return count;
     }
 }
