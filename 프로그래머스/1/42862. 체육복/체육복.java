69da05901893c954380487f6f02c4267f2f7c87a
drago <138676687+Firedrago95@users.noreply.github.com>
2026-03-15 13:52:47 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,27 +1,35 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] status = new int[n];
+        int[] students = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            students[i] = 1;
+        }
         
-        for (int l : lost) status[l - 1]--;
-        for (int r : reserve) status [r - 1]++;
+        for (int l : lost) {
+            students[l]--;
+        }
         
-        for (int i = 0; i < n; i++) {
-            if (status[i] == -1) {
-                if (i > 0 && status[i - 1] == 1) {
-                    status[i]++;
-                    status[i - 1]--;
-                } else if (i < n - 1 && status[i + 1] == 1) {
-                    status[i]++;
-                    status[i + 1]--;
-                }
+        for (int r : reserve) {
+            students[r]++;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            if (i > 1 && students[i - 1] == 0 && students[i] == 2) {
+                students[i-1]++;
+                students[i]--;
+                continue;
+            }
+            if (i <= n - 1 && students[i + 1] == 0 && students[i] == 2) {
+                students[i + 1]++;
+                students[i]--;
             }
         }
         
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            if (status[i] > -1) answer++; 
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (students[i] >= 1) count++;
         }
-        return answer;
+        return count;
     }
 }
