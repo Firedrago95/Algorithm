64451c484127507834911b1d027d67e6bd45aebf
drago <138676687+Firedrago95@users.noreply.github.com>
2026-03-21 15:15:16 +0900

    [level 2] Title: 올바른 괄호, Time: 19.35 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,11 +1,11 @@
 import java.util.*;
 class Solution {
     boolean solution(String s) {
-        Deque<Integer> stack = new ArrayDeque<>();
+        Stack<Integer> stack = new Stack<>();
         
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.push(0);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
             } else {
                 if (stack.isEmpty()) return false;
                 stack.pop();
