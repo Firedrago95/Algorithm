3ab7f69fe36a66ca83c8b809701c24ae3607e699
drago <138676687+Firedrago95@users.noreply.github.com>
2026-03-21 16:05:46 +0900

    [level 2] Title: 프로세스, Time: 1.32 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,20 +1,24 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
+        Queue<int[]> q = new LinkedList<>();
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int p : priorities) {
-            pq.add(p);
+        
+        for (int i = 0; i < priorities.length; i++) {
+            q.add(new int[]{priorities[i], i});
+            pq.add(priorities[i]);
         }
+        
         int count = 0;
-        while(!pq.isEmpty()) {
-            for (int i = 0; i < priorities.length; i++) {
-                if (pq.peek() == priorities[i]) {
-                    count++;
-                    pq.poll();
-                    if (i == location) {
-                        return count;
-                    }
-                }
+        while(!q.isEmpty()) {
+            int[] poll = q.poll();
+            
+            if (poll[0] == pq.peek()) {
+                count++;
+                pq.poll();
+                if (poll[1] == location) return count;
+            } else {
+                q.add(poll);
             }
         }
         return count;
