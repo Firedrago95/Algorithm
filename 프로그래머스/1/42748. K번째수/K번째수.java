813a62964611e33b740e9b12f004803450c35d65
drago <138676687+Firedrago95@users.noreply.github.com>
2026-03-23 12:40:46 +0900

    [level 1] Title: K번째수, Time: 1.14 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,15 +4,16 @@ class Solution {
         int[] answer = new int[commands.length];
         
         for (int i = 0; i < commands.length; i++) {
-            int[] list = new int[commands[i][1] - commands[i][0] + 1];
+            int[] nums = new int[commands[i][1] - commands[i][0] + 1];
             int k = 0;
+            
             for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
-                list[k++] = array[j];
+                nums[k++] = array[j];
             }
-                                 
-            Arrays.sort(list);
-                                 
-            answer[i] = list[commands[i][2] - 1];
+            
+            Arrays.sort(nums);
+            
+            answer[i] = nums[commands[i][2] - 1];
         }
         return answer;
     }
