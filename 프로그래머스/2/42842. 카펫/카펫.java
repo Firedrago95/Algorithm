9ab5f137cbe470edf1ecf9cfe1235981b9c2cb8b
drago <138676687+Firedrago95@users.noreply.github.com>
2026-03-27 15:01:13 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,12 +1,12 @@
 import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
-        for (int h = 1; h <= Math.sqrt(yellow); h++) {
-            if (yellow % h == 0) {
-                int w = yellow / h;
+        for (int i = 1; i <= Math.sqrt(yellow); i++) {
+            if (yellow % i == 0) {
+                int j = yellow / i;
                 
-                if ((h * 2) + (w * 2) + 4 == brown) {
-                    return new int[]{w + 2, h + 2};
+                if (i * 2 + j * 2 + 4 == brown) {
+                    return new int[] {j + 2, i + 2};
                 }
             }
         }
