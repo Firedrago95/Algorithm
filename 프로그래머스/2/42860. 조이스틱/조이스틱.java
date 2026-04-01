7db6e680d7de462fab289e71c7d9f473e29b26f2
drago <138676687+Firedrago95@users.noreply.github.com>
2026-04-01 19:36:51 +0900

    [level 2] Title: 조이스틱, Time: 0.07 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -2,11 +2,11 @@ import java.util.*;
 class Solution {
     public int solution(String name) {
         char[] names = name.toCharArray();
-        int upDown = 0;
+        int up = 0;
         int leftRight = names.length - 1;
         
         for (int i = 0; i < names.length; i++) {
-            upDown += Math.min('Z' - names[i] + 1, names[i] - 'A');
+            up += Math.min('Z' - names[i] + 1, names[i] - 'A');
             
             int index = i + 1;
             while (index < names.length && names[index] == 'A') index++;
@@ -15,6 +15,6 @@ class Solution {
             leftRight = Math.min(leftRight, (names.length - index) * 2 + i);
         }
         
-        return upDown + leftRight;
+        return up + leftRight;
     }
 }
