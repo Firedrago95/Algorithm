de93b0dac04d74f7988482b75292675d5dfc7e3d
drago <138676687+Firedrago95@users.noreply.github.com>
2026-04-02 17:59:13 +0900

    [level 3] Title: 단속카메라, Time: 11.40 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,19 +1,18 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 0;
+        // 1. 나가는 지점 오름차순 정렬
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
         
-        Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
-        
-        int cameraPos = -30001;
-        
+        int count = 0;
+        int lastCamera = -30001;
         for (int[] route : routes) {
-            if (route[0] > cameraPos) {
-                cameraPos = route[1];
-                answer++;
+            // 현재 카메라가 진입점 보다 앞에 있을경우
+            if (lastCamera < route[0]) {
+                count++;
+                lastCamera = route[1]; // 이 차량이 나가는 지점에 카메라 설치
             }
         }
-        
-        return answer;
+        return count;
     }
 }
