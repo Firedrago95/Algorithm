906ac469e7334ca2e63441bf63013efa599c78d4
drago <138676687+Firedrago95@users.noreply.github.com>
2026-04-03 13:23:51 +0900

    [level 3] Title: 정수 삼각형, Time: 12.01 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -3,24 +3,23 @@ class Solution {
     public int solution(int[][] triangle) {
         int n = triangle.length;
         int[][] dp = new int[n][n];
-        
         dp[0][0] = triangle[0][0];
+        
         for (int i = 1; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 if (j == 0) {
                     dp[i][j] = dp[i - 1][j] + triangle[i][j];
-                } else if (j == n - 1) {
+                } else if (j == i) {
                     dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
                 }
             }
         }
-        
-        int max = 0;
-        for (int j = 0; j < n; j++) {
-            max = Math.max(max, dp[n - 1][j]);
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result = Math.max(result, dp[n - 1][i]);
         }
-        return max;
+        return result;
     }
 }
