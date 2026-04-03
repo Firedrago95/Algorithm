213fe602822175fbf24278719831d170bf10876d
drago <138676687+Firedrago95@users.noreply.github.com>
2026-04-03 12:36:39 +0900

    [level 3] Title: N으로 표현, Time: 35.30 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,37 +1,36 @@
 import java.util.*;
 class Solution {
     public int solution(int N, int number) {
-        Set<Integer>[] set = new Set[9];
+        // 1. N과 number가 일치하면 바로 1 반환
+        if (N == number) return 1;
         
-        for (int i = 1; i <= 8; i++) {
-            set[i] = new HashSet<>();
+        // 2. 8번 까지만 결과 저장하기위한 List<Set<Integer>> 생성
+        List<Set<Integer>> list = new ArrayList<>();
+        for (int i = 0; i <= 8; i++) {
+            list.add(new HashSet<>());
         }
         
+        // 3. 단순 N 반복 숫자 초기화
+        int targetN = N;
         for (int i = 1; i <= 8; i++) {
-            set[i].add(getNM(N, i));
-            
+            list.get(i).add(targetN);
+            targetN = targetN * 10 + N;
+        }
+        
+        // 4. dp식 사용 초기화
+        for (int i = 1; i <= 8; i++) {
             for (int j = 1; j < i; j++) {
-                int k = i - j;
-                for (int s : set[j]) {
-                    for (int f : set[k]) {
-                        set[i].add(f + s);
-                        set[i].add(f * s);
-                        set[i].add(f - s);
-                        if (s != 0) set[i].add(f / s);
+                for (int op1 : list.get(j)) {
+                    for (int op2 : list.get(i - j)) {
+                        list.get(i).add(op1 + op2);
+                        list.get(i).add(op1 - op2);
+                        list.get(i).add(op1 * op2);
+                        if (op2 != 0) list.get(i).add(op1 / op2);
                     }
                 }
             }
-            
-            if (set[i].contains(number)) return i;
+            if (list.get(i).contains(number)) return i;
         }
         return -1;
-    }
-    
-    private int getNM(int N, int count) {
-        int a = 0;
-        while(count-- > 0) {
-            a = a * 10 + N;
-        }
-        return a;
     }
 }
