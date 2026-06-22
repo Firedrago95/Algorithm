b64812adb65004126e421da2561c685e7e73ae40
drago <138676687+Firedrago95@users.noreply.github.com>
2026-06-22 16:53:14 +0900

    [level 2] Title: 피로도, Time: 1.51 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,26 +1,26 @@
 import java.util.*;
 class Solution {
-    boolean[] visited;
-    int count = 0;
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        
-        backTrack(dungeons, k, 0);
-        
-        return count;
+        boolean[] visited = new boolean[dungeons.length];
+        return backTracking(dungeons, visited, k, 0);
     }
     
-    public void backTrack(int[][] dungeons, int k, int depth) {
-        count = Math.max(count, depth);
-        
-        if (depth == dungeons.length) return;
+    public int backTracking(
+        int[][] dungeons,
+        boolean[] visited, 
+        int currentFatigue, 
+        int count
+    ) {
+        int maxCount = count;
         
         for (int i = 0; i < dungeons.length; i++) {
-            if (!visited[i] && k >= dungeons[i][0]) {
+            if (!visited[i] && currentFatigue >= dungeons[i][0]) {
                 visited[i] = true;
-                backTrack(dungeons, k - dungeons[i][1], depth + 1);
+                maxCount = Math.max(maxCount, backTracking(dungeons, visited, currentFatigue - dungeons[i][1], count + 1));
                 visited[i] = false;
             }
         }
+        
+        return maxCount;
     }
 }
