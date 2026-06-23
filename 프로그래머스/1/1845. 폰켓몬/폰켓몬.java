04d70f6b22418c8c988640b7ab3f5ed06655d900
drago <138676687+Firedrago95@users.noreply.github.com>
2026-06-23 10:40:17 +0900

    [level 1] Title: 폰켓몬, Time: 2.13 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,12 +1,23 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
+        // 1. 총 종류의 개수 = nums.length
+        int n = nums.length / 2;
+        
+        // 2. 중복 없는 종류의 개수 set.size()
         Set<Integer> set = new HashSet<>();
-        
-        for (int n : nums) {
-            set.add(n);
+        for (int num : nums) {
+            set.add(num);
         }
+        int setSize = set.size();
+
         
-        return Math.min(nums.length / 2 , set.size());
+        // 2-1 구하려고 하는 개수가 총 종류보다 크거나 같을때 n/2 > set.size()
+        // 3개를 구해야 하는데 종류가 2개 => 2개 (set.size())
+        if (n >= setSize) return setSize;
+        
+        // 2-2 구하려고 하는 개수가 총 종류보다 작거나 같을때 n/2 <= set.size()
+        // 2개를 구해야 하는데 총 종류가 2개 or 3개 => 2개 (n/2 개)
+        return n;
     }
 }
