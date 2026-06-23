0e24049f8fef2f81358088643ddbb0fc05fa4b91
drago <138676687+Firedrago95@users.noreply.github.com>
2026-06-23 12:17:31 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,24 +1,25 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
-        int networkCount = 0;
-        boolean[] visited = new boolean[n];
-        
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(computers, visited, i);
-                networkCount++;
+        int totalNetworks = 0;
+        boolean[] visited = new boolean[computers.length];
+        for (int i = 0; i < computers.length; i++) {
+            for (int j = 0; j < computers[i].length; j++) {
+                if (!visited[j]) {
+                    dfs(computers, visited, j);
+                    totalNetworks++;
+                }
             }
         }
-        return networkCount;
+        return totalNetworks;
     }
     
-    public void dfs (int[][] computers, boolean[] visited, int i) {
-        visited[i] = true;
+    public void dfs (int[][] computers, boolean[] visited, int current) {
+        visited[current] = true;
         
-        for (int j = 0; j < computers[i].length; j++) {
-            if (i != j && !visited[j] && computers[i][j] == 1) {
-                dfs(computers, visited, j);
+        for (int i = 0; i < computers[current].length; i++) {
+            if (i != current && !visited[i] && computers[current][i] == 1) {
+                dfs(computers, visited, i);
             }
         }
     }
