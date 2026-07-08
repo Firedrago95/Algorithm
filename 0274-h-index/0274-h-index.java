6d4bf463392b8584f27176529c6ef13673931567
drago <138676687+Firedrago95@users.noreply.github.com>
2026-07-08 11:18:13 +0900

    Time: 0 ms (100%), Space: 43 MB (96.08%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,13 +1,20 @@
 class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
+        int[] arr = new int[n + 1];
 
-        Arrays.sort(citations);
-
-        for (int i = 0; i < n; i++) {
-            if (citations[i] >= n - i) {
-                return n - i;
+        for (int c : citations) {
+            if (c >= n) {
+                arr[n]++;
+            } else {
+                arr[c]++;
             }
+        }
+
+        int total = 0;
+        for (int i = n; i >= 0; i--) {
+            total += arr[i];
+            if (total >= i) return i;
         }
         return 0;
     }
