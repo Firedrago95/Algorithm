39415130887567f13ff3f057413c6d1987d71123
drago <138676687+Firedrago95@users.noreply.github.com>
2026-08-29 10:01:08 +0900

    Time: 2 ms (70.86%), Space: 55.5 MB (66.45%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -10,7 +10,7 @@ class Solution {
                 count--;
             }
 
-            if (count < 0) {
+            if (count <= 0) {
                 result = nums[i];
                 count = 1;
             }
