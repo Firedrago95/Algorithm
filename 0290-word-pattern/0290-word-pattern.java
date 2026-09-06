95e553f639da0198c986d4999050177207565c7b
drago <138676687+Firedrago95@users.noreply.github.com>
2026-09-06 09:12:03 +0900

    Time: 0 ms (100%), Space: 42.9 MB (36.33%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Set<String> seenWords = new HashSet<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            if (charToWord.containsKey(c)) {
+                if (!charToWord.get(c).equals(word)) {
+                    return false;
+                }
+            } else {
+                if (seenWords.contains(word)) {
+                    return false;
+                }
+                charToWord.put(c, word);
+                seenWords.add(word);
+            }
+        }
+        return true;
+    }
+}
