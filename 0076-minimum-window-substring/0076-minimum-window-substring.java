f2500487eb90346bb891d2ad8b21d72787cd5df9
drago <138676687+Firedrago95@users.noreply.github.com>
2026-09-14 10:45:12 +0900

    Time: 2 ms (99.68%), Space: 45.6 MB (75.38%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -2,37 +2,37 @@ class Solution {
     public String minWindow(String s, String t) {
         if (s.length() < t.length()) return "";
 
-        int[] count = new int[128];
+        int[] count = new int[126];
         for (char c : t.toCharArray()) {
             count[c]++;
         }
-        
-        int minLength = Integer.MAX_VALUE;
+
         int required = t.length();
+        int min = Integer.MAX_VALUE;
         int left = 0;
         int start = 0;
 
         for (int right = 0; right < s.length(); right++) {
-            char rChar = s.charAt(right);
-            if (count[rChar] > 0) {
+            char rc = s.charAt(right);
+            if (count[rc] > 0) {
                 required--;
             }
-            count[rChar]--;
+            count[rc]--;
 
             while (required == 0) {
-                if (right - left + 1 < minLength) {
-                    minLength = right - left + 1;
+                if (right - left + 1 < min) {
+                    min = right - left + 1;
                     start = left;
                 }
 
-                char lChar = s.charAt(left);
-                count[lChar]++;
-                if (count[lChar] > 0) {
+                char lc = s.charAt(left);
+                count[lc]++;
+                if (count[lc] > 0) {
                     required++;
                 }
                 left++;
             }
         }
-        return minLength == Integer.MAX_VALUE ? "" : s.substring(start, start + minLength);
+        return min == Integer.MAX_VALUE ? "" : s.substring(start, start + min);
     }
 }
